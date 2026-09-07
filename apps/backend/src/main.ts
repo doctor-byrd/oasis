4e0a24af80bb1de64043c4e2281ed4c0bbeb90ca
Bird <doctor.byrd@proton.me>
2026-09-06 22:06:32 -0400

    Updated

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,14 +3,36 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { updateGlobalConfig } from 'nestjs-paginate';
+import { PaginationOptions } from './common/general';
+import { RedisIoAdapter } from './adapters/redis.adapter';
+
+// Global pagination configurations
+updateGlobalConfig({
+  defaultOrigin: PaginationOptions.DEFAULT_ORIGIN as string,
+  defaultLimit: PaginationOptions.DEFAULT_LIMIT as number,
+  defaultMaxLimit: PaginationOptions.DEFAULT_MAX_LIMIT as number,
+})
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  // Initialize and connect to Redis
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+  app.useWebSocketAdapter(redisIoAdapter);
+
+  // Additional configurations
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
